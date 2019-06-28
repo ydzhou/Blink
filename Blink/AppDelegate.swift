daaeeceb5c02c956c54f2b64a74e7d0a13f24831
ydzhou <ydotzhou@gmail.com>
2019-06-28 00:21:39 -0700

    Add open function in menu and can open image in different window

--- Blink/AppDelegate.swift
+++ Blink/AppDelegate.swift
@@ -19,5 +19,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
+    @IBAction func open(_ sender: AnyObject) {
+        var myWindow: NSWindow? = nil
+        let storyboard = NSStoryboard(name: "Main",bundle: nil)
+        let controller: MainViewController = storyboard.instantiateController(withIdentifier: "MainViewController") as! MainViewController
+        myWindow = NSWindow(contentViewController: controller)
+        myWindow?.makeKeyAndOrderFront(self)
+        let vc = NSWindowController(window: myWindow)
+        vc.showWindow(self)
+    }
+
 }
 
